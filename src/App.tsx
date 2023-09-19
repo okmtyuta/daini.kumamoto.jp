d9d3feedd069bc25a49d7736814b4172ab0ff2a7
okmtyuta <okmtyuta@gmail.com>
2023-09-19 22:42:36 +0900

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './reset.css'
 import { Header } from './components/header/Header'
 import { Footer } from './components/footer/Footer'
-import { Frame, Typography, Link } from '@okmtyuta/amatelas/server'
+import {
+  Frame,
+  Typography,
+  Link,
+  Paragraph,
+  Space
+} from '@okmtyuta/amatelas/server'
 import '@okmtyuta/amatelas/server/index.css'
 
 import styles from './app.module.css'
@@ -10,14 +16,26 @@ export const App = () => {
   return (
     <div className={styles['app']}>
       <Header className={styles['header']} />
-      <Frame className={styles["frame"]}>
-        <Typography fontSize="heading2">
-          Welcome!! This page is managed by{' '}
-          <Link href="https://mirror.me.okmtyuta.jp" underlined>
-            okmtyuta
-          </Link>
-          .
-        </Typography>
+      <Frame className={styles['frame']}>
+        <Paragraph>
+          <Typography fontSize="heading2">
+            Welcome!! This page is managed by
+            <Space />
+            <Link href="https://mirror.me.okmtyuta.jp" underlined>
+              okmtyuta
+            </Link>
+            .
+          </Typography>
+        </Paragraph>
+        <Paragraph>
+          <Typography fontSize="heading2">
+            Repository is:
+            <Space />
+            <Link underlined href="https://github.com/okmtyuta/daini.kumamoto.jp">
+              daini.kumamoto.jp
+            </Link>
+          </Typography>
+        </Paragraph>
       </Frame>
       <Footer className={styles['footer']} />
     </div>
